256aec06b2155bbdbb0ea4f6a351237973182852
beartums <eric@griffithnet.com>
2019-06-02 11:07:46 +0300

    add files for messages icon and favicon

---
+++ src/app/messages-dialog/messages-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-messages-dialog',
+  templateUrl: './messages-dialog.component.html',
+  styleUrls: ['./messages-dialog.component.css']
+})
+export class MessagesDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
