4131eb8d208fea001bb8ab7ec36502d6a0011b59
beartums <eric@griffithnet.com>
2019-06-08 13:01:16 +0300

    unclean pipe and environment changes

---
+++ src/app/unclean-html.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { UncleanHtmlPipe } from './unclean-html.pipe';
+
+describe('UncleanHtmlPipe', () => {
+  it('create an instance', () => {
+    const pipe = new UncleanHtmlPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
